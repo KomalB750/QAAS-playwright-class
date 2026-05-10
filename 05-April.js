9e99baec398cee66b02eda0f5c3586e0fe93e528
KomalBaseshankar <baseshankarkomal@gmail.com>
2026-05-10 12:01:51 +0530

    first commit

---
+++ 05-April.js
@@ -0,0 +1,236 @@
+//Array
+// let name1 = 'Komal'
+// let name2 = 'Bhavesh'
+// let name3 = 'Monu'
+// let name4 = 'Radha'
+// let name5 = 'priti'
+
+// //let names = ['Komal','Sonu','Monu', 'Radha','priti']
+// //console.log(names[0])
+
+// let fruits = ["Mango", "Banana"]
+// console.log(fruits)
+
+// fruits.push('Grapes')
+// console.log(fruits)
+
+// fruits.pop()
+// console.log(fruits)
+
+// console.log(fruits.length)
+// console.log(fruits.includes("Banana"))
+// console.log(fruits.includes("Kiwi"))
+
+// console.log(fruits.indexOf("Banana"))
+
+// //task
+// let names = ['Komal','Sonali', 'Gulshan']
+
+// names.push('Bhavesh')
+// console.log(names)
+
+// console.log(names.includes("Bhavesh"))
+
+// names.pop()
+// console.log(names)
+
+// names.push('Bhavesh')
+// console.log(names)
+
+// console.log(names.indexOf("Komal"))
+
+// //objects
+
+// let user = {
+//     name : "Komal",
+//     age : 33,
+//     city : "Nagpur",
+//     amIsweet : true,
+//     skills : ["python" , "javascript"]
+// }
+
+// console.log(user.skills)
+// console.log(Object.keys(user))
+// console.log(Object.values(user))
+// console.log(Object.entries(user))
+
+// //task
+// let response = {
+//     status : "success",
+//     message : "Login Scussessfull",
+//     data : user
+// }
+
+// console.log(response)
+
+// let age = 18
+
+// if(age > 18){
+//     console.log("you are eligible")
+// }else{
+//     console.log("you are not eligible")
+// }
+
+// let status = "success"
+
+// if(status == "success"){
+//     console.log("code ran successfully")
+// }else{
+//     console.log("code gave an error")
+// }
+
+//console.log(names.indexOf("Komal"))
+//task
+
+// let numbers = [2,3]
+// let operation = "+"
+
+// if(operation === "+"){
+//     console.log(numbers[0]+numbers[1])
+// }
+
+// if (operation === "+"){
+//     let add = numbers[0] + numbers[1]
+//     console.log(add)
+// }
+// else if (operation === "-")
+//     {
+//     let sub = numbers[0] - numbers[1]
+//     console.log(sub)
+
+// }else if (operation === "*"){
+//     let mul = numbers[0] * numbers[1]
+//     console.log(mul)
+
+// }else if (operation === "/"){
+//     let div = numbers[0] / numbers[1]
+//     console.log(div)
+// }
+
+//Array
+// let name1 = "Riya"
+// let name2 = "Komal"
+// let name3 = "Pihyu"
+// let name4 = "Janvi"
+
+// let names = ["Riya", "Komal", "Pihyu", "Janvi"]
+// console.log(names[0])
+// console.log(names[1])
+// console.log(names[2])
+// console.log(names[3])
+
+// fruits = ["Mango", "Banana"]
+// console.log(fruits)
+
+// fruits.push("Grapes")//Adds the element in the array
+// console.log(fruits)
+
+// fruits.pop()//remove element from the array at the end
+// console.log(fruits)
+
+// console.log(fruits.length)
+
+// let res = fruits.push("Kiwi")
+// console.log(res)
+
+// console.log(fruits.includes("Mango"))
+
+// console.log(fruits.indexOf("Grapes"))
+
+let names = ["Riya", "Komal", "Pihyu", "Janvi"]
+
+names.push("Bhavesh")
+console.log(names)
+
+console.log(names.includes("Bhavesh"))
+
+names.pop()
+console.log(names)
+
+names.push("Bhavesh")
+console.log(names)
+
+console.log(names.indexOf("Komal"))
+
+console.log(names.length)
+
+// -------------------------Object---------Dictionary in python-------------
+//left is key and right is value
+
+let user = {
+    name : "Komal",
+    age : 34,
+    state : "Maharashtra",
+    city : "Nagpur",
+    amIsweet : true,
+    skills : ["javascrpits", "python", "jave"]
+}
+
+console.log(user.skills)
+console.log(user.city)
+
+console.log(Object.keys(user))
+console.log(Object.values(user))
+console.log(Object.entries(user))
+
+//task
+res = {
+    status : "success",
+    message : "Login Successfull",
+    data : user
+}
+
+console.log(res)
+console.log(Object.keys(res))
+console.log(Object.values(res))
+console.log(Object.entries(res))
+
+//-------------------conditionals-------------------
+
+// if --else
+
+let age = 18
+
+if (age >= 20){
+    console.log("your are eligible for license")
+}
+else{console.log("your are eligible for license")
+
+}
+
+// task
+
+let status = "success"//success, failure, pending
+
+if (status == "success"){
+    console.log("code ran scccessfully")
+}
+else if(status == "failure"){
+    console.log("code gave an error")
+}
+else if(status == "pending"){
+    console.log("code is still pending")
+}
+else{
+    console.log("I don't know")
+}
+
+// Operators 
+// AND
+// OR
+
+// True AND True = True
+// True AND False = False
+// True OR True = True
+// True OR False = True
+// False OR False = False
+
+// 18>= 18 && 20>=18 // true && true ->true
+// 17>= 18 && 20>=18 // true && true ->false
+
+
+
+
+
+    
+    
