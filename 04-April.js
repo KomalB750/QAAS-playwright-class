c3cc64337614ce3cc8fc24324c1f14783d1744bc
KomalBaseshankar <baseshankarkomal@gmail.com>
2026-05-10 12:10:17 +0530

    adding 04-April file

---
+++ 04-April.js
@@ -0,0 +1,43 @@
+//let name = "komal";
+//let age = "100";
+//let state = "Maharashtra"
+
+//console.log(name)
+
+//rules of variables
+//let address = "Nagpur"
+//let _street = "stree2"
+///let $state = "Maharashtra"
+
+//message = "My name is " + name + " I am software Engineer"
+//console.log(message)
+
+//message = "Hello my name is " + name + " My age is "+ age + " I am belogs to "+ state
+//console.log(message)
+
+//task
+// let error = "Your password is incorrect"
+// let category = "UnAuthenticate"
+
+//let response = "Error : [ " + category + " ] : " + error
+//console.log(response)
+//Error : [UnAuthenticate] : Your password is incorrect
+
+///let response = `Error : [${category}] : ${error}`
+//console.log(response)
+
+let name = "komal";
+let place = "Nagpur";
+let item = "flowers"
+
+let response = `Hi My name is ${name}, I live in ${place}, I love ${item}`
+console.log(response)
+
+ 
+let Principal = 30
+let time = 2
+let rate = 4.5
+
+let Simple_Interest = Principal * time * rate /100
+console.log(Simple_Interest)
+
